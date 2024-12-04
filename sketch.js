0a72f07fd78de1985b78d827ae09326fe0c05ba0
orangemoji <noe.naranjoarias@sjsu.edu>
2024-12-03 21:26:11 -0800

    updated bae

---
+++ sketch.js
@@ -0,0 +1,48 @@
+function draw() {
+  background(220);
+
+  // Title text
+  fill(0);
+  textSize(24);
+  textAlign(CENTER);
+  text("Catch the Burrito", width / 2, 30);
+
+  // Scoreboard
+  textSize(16);
+  text("Score: " + score, width / 2, 60);
+
+  // Taco (player)
+  fill(255, 200, 0);
+  rect(tacoX, tacoY, 50, 20);
+
+  // Burrito (falling object)
+  fill(180, 90, 50);
+  ellipse(burritoX, burritoY, 30, 15);
+
+  // Move burrito
+  burritoY += burritoSpeed;
+
+  // Check for catch
+  if (burritoY > tacoY && burritoX > tacoX && burritoX < tacoX + 50) {
+    burritoY = 0;
+    burritoX = random(width);
+    score++; // Increase score
+  }
+
+  // Reset burrito if it misses
+  if (burritoY > height) {
+    burritoY = 0;
+    burritoX = random(width);
+  }
+
+  // Move taco
+  if (keyIsDown(LEFT_ARROW)) {
+    tacoX -= 5;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    tacoX += 5;
+  }
+
+  // Keep taco in bounds
+  tacoX = constrain(tacoX, 0, width - 50);
+}
